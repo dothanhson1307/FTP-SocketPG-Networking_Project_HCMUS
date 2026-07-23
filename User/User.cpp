03a99006a5cf1d645c8c9079ac26fe6435ae7dbe
vukiet <xirackss@gmail.com>
2026-07-23 12:38:54 +0000

    feat(User): add and external vector Accounts for storing

--- User/User.cpp
+++ User/User.cpp
@@ -5,3 +5,8 @@ User::User(string n,string p):username(n),password(p){}
 string User::getUsername(){return username;}
 
 string User::getPassword(){return password;}
+
+std::vector<User> Accounts = {
+    User("son", "son123"),
+    User("kiet", "kiet123")
+};
