362e8f7955d058b00504e677795eb6d5808133c4
vukiet <xirackss@gmail.com>
2026-08-03 08:30:44 +0000

    tra lai cho DO SON nhung gi da mat

--- Command/Integrity/Hash.cpp
+++ Command/Integrity/Hash.cpp
@@ -1,5 +1,5 @@
 #include "Hash.h"
-#include <openssl/sha.h>
+#include <CommonCrypto/CommonDigest.h>
 #include <fstream>
 #include <iomanip>
 #include <sstream>
@@ -10,19 +10,19 @@ string calculateFileSHA256(const string& filepath) {
         return "";
     }
 
-    SHA256_CTX sha256;
-    SHA256_Init(&sha256);
+    CC_SHA256_CTX sha256;
+    CC_SHA256_Init(&sha256);
 
     char buffer[4096];
     while (fin.read(buffer, sizeof(buffer)) || fin.gcount() > 0) {
-        SHA256_Update(&sha256, buffer, static_cast<uint32_t>(fin.gcount()));
+        CC_SHA256_Update(&sha256, buffer, static_cast<CC_LONG>(fin.gcount()));
     }
 
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256_Final(hash, &sha256);
+    unsigned char hash[CC_SHA256_DIGEST_LENGTH];
+    CC_SHA256_Final(hash, &sha256);
 
     std::stringstream ss;
-    for (int i = 0; i < SHA256_DIGEST_LENGTH; ++i) {
+    for (int i = 0; i < CC_SHA256_DIGEST_LENGTH; ++i) {
         ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(hash[i]);
     }
 
